b376ec01356c5ba8487268562dbfec893e595854
Jomchen <937574740@qq.com>
2018-01-12 22:54:15 +0800

    提交项目变更

--- src/main/java/com/jomchen/springtest/test/AllTest.java
+++ src/main/java/com/jomchen/springtest/test/AllTest.java
@@ -1,13 +1,21 @@
 package com.jomchen.springtest.test;
 
-import com.jomchen.springtest.entity.basedata.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * create by Jomchen on 2018/1/5
  */
 public class AllTest {
     public static void main(String[] args) {
-        Customer customer = new Customer();
-
+        List<String> list = new ArrayList<>();
+        list.add("5");
+        list.add("4");
+        list.add("3");
+        list.add("2");
+        list.add("1");
+        list.add("0");
+        list.stream().forEach(System.out::println);
     }
 }
---
+++ src/test/java/com/jomchen/springtest/MyTest.java
@@ -0,0 +1,7 @@
+package java.com.jomchen.springtest;
+
+/**
+ * create by Jomchen on 18-1-12
+ */
+public class MyTest {
+}
