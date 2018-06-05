0ea59b39a4bdb9f124d74b824b818abc7a68086d
Jomchen <937574740@qq.com>
2018-06-05 15:41:26 +0800

    增加了对不依赖xml的对路径扫描

--- src/main/java/com/jomchen/springtest/test/AllTest.java
+++ src/main/java/com/jomchen/springtest/test/AllTest.java
@@ -1,11 +1,14 @@
 package com.jomchen.springtest.test;
 
 
+import com.jomchen.springtest.commons.factorys.OneFactory;
 import com.jomchen.springtest.commons.factorys.TwoFactory;
 import com.jomchen.springtest.entity.basedata.Customer;
+import com.jomchen.springtest.entity.basedata.Kongfu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.time.format.DateTimeFormatter;
@@ -20,9 +23,14 @@ public class AllTest {
     private static DateTimeFormatter DATE_FOR_MATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-core.xml");
+        /*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-core.xml");*/
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(TwoFactory.class, OneFactory.class);
+        applicationContext.refresh();
         TwoFactory twoFactory = applicationContext.getBean("twoFactory", TwoFactory.class);
-        LOGGER.warn("信息为：{}", twoFactory.getCustomer());
+        LOGGER.warn("信息为：{}", twoFactory.getCustomer().getCname());
 
+
+        applicationContext.close();
     }
 }
