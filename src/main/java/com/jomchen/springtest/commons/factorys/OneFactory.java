d00785823eeae632cd7fce6c9cde7f4c96882cfe
Jomchen <937574740@qq.com>
2018-06-05 17:03:05 +0800

    配置 web.xml

--- src/main/java/com/jomchen/springtest/commons/factorys/OneFactory.java
+++ src/main/java/com/jomchen/springtest/commons/factorys/OneFactory.java
@@ -3,6 +3,7 @@ package com.jomchen.springtest.commons.factorys;
 import com.jomchen.springtest.entity.basedata.Customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 
 /**
  * create by Jomchen on 2018/5/29
@@ -12,6 +13,7 @@ public class OneFactory {
 
 
     @Bean
+    @Description("这个只是我用来测试由java容器生成的类由spring管理")
     public Customer factoryCustomer() {
         Customer bean = new Customer();
         bean.setCid(0);
