8a24410da6f811e8dab4bede2188edb5e6311526
Jomchen <937574740@qq.com>
2018-06-16 13:54:35 +0800

    增加一个controller

--- src/main/java/com/jomchen/springtest/controllers/basedata/CustomerController.java
+++ src/main/java/com/jomchen/springtest/controllers/basedata/CustomerController.java
@@ -1,10 +1,27 @@
 package com.jomchen.springtest.controllers.basedata;
 
 import com.jomchen.springtest.controllers.common.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * create by Jomchen on 2018/1/6
  */
+@RequestMapping(value = "/customerController")
+@Controller
 public class CustomerController extends BaseController {
+
+
+    @RequestMapping("testCustomerController")
+    @ResponseBody
+    public String testCustomerCOntroller() {
+        return "Hellow Linux";
+    }
+
+    @RequestMapping("testPage")
+    public String testPage() {
+        return "test/test";
+    }
 
 }
