7f05d1ccdb20401de2f4a2a1dbb39bb68212d3d8
Jomchen <937574740@qq.com>
2018-06-27 14:08:52 +0800

    做了一些微调

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -6,6 +6,7 @@
         <title>主页</title>
         <link rel="stylesheet" type="text/css" href="./static/indexs/css/index.css">
         <script type="text/javascript" src="./static/common/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="./static/common/js/sockjs.min.js"></script>
         <script type="text/javascript" src="./static/indexs/js/index.js"></script>
     </head>
 
--- src/main/webapp/static/indexs/js/index.js
+++ src/main/webapp/static/indexs/js/index.js
@@ -15,12 +15,12 @@ socket.onmessage = function(message) {
 socket.onclose = function() {
     var state = checkState(socket.readyState);
     console.log(state);
-    console.log("webSocket client disconnected !!")
+    console.log("webSocket client 连接断开。。")
 };
 socket.onerrror = function() {
     var state = checkState(socket.readyState);
     console.log(state);
-    console.log("webSocket client error");
+    console.log("webSocket client 错误 ！！");
 };
 window.onbeforeunload = function() {
   socket.close();
@@ -30,19 +30,19 @@ function checkState(state) {
     var result;
     switch(state) {
         case WebSocket.CONNECTING:
-            result = "正在连接";
+            result = "webSocket 正在连接";
             break;
         case WebSocket.OPEN:
-            result = "连接成功";
+            result = "webSocket 连接成功";
             break;
         case WebSocket.CLOSING:
-            result = "关闭中";
+            result = "webSocket 关闭中";
             break;
         case WebSocket.CLOSED:
-            result = "已经关闭";
+            result = "webSocket 已经关闭";
             break;
         default:
-            result = "未知状态";
+            result = "webSocket 状态未知";
             break;
     }
     return result;
@@ -55,3 +55,24 @@ $(function() {
         socket.send(data);
     });
 });
+
+/ * ----------------------------------------------------------------------------------------------------------------- */
+
+/*
+var sockjs = new SockJS("http://127.0.0.1:8080/getMyTextHandler");
+sockjs.onopen = function() {
+    console.log("sockJs 打开连接了。。。");
+};
+sockjs.onmessage = function(message) {
+    console.log("sockJs 接收到的消息为：" + message.data);
+};
+sockjs.onclose = function() {
+    console.log("sockJs 连接断开了。。。");
+};
+
+$(function() {
+    $(".submit").click(function () {
+        var data = prompt("请输入您要上传的字符串？", "Linux");
+        sockjs.send(data);
+    });
+});*/
