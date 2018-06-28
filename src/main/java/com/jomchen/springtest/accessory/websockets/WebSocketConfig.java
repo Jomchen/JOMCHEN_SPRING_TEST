410bcc65ad00490276dfbf5203a479bd30e3b31d
Jomchen <937574740@qq.com>
2018-06-28 10:00:06 +0800

    增加了一个对握手之前和握手之后的处理是

--- src/main/java/com/jomchen/springtest/accessory/websockets/WebSocketConfig.java
+++ src/main/java/com/jomchen/springtest/accessory/websockets/WebSocketConfig.java
@@ -2,11 +2,17 @@ package com.jomchen.springtest.accessory.websockets;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import java.util.Map;
 
 /**
  * create by Jomchen on 2018/6/25
@@ -25,15 +31,42 @@ public class WebSocketConfig implements WebSocketConfigurer{
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        HttpSessionHandshakeInterceptor httpSessionHandshakeInterceptor = new HttpSessionHandshakeInterceptor();
         webSocketHandlerRegistry.addHandler(getMyTextHandler(), "/getMyTextHandler")
                 .setAllowedOrigins("*")
-                .addInterceptors(httpSessionHandshakeInterceptor);
+                .addInterceptors(httpSessionHandshakeInterceptor());
     }
 
     @Bean
     public MyTextHandler getMyTextHandler() {
         return new MyTextHandler();
+    }
+
+    @Bean
+    public HttpSessionHandshakeInterceptor httpSessionHandshakeInterceptor() {
+        return new HttpSessionHandshakeInterceptor() {
+            @Override
+            public boolean beforeHandshake(
+                    ServerHttpRequest request,
+                    ServerHttpResponse response,
+                    WebSocketHandler wsHandler,
+                    Map<String, Object> attributes) throws Exception {
+                System.out.println("执行了握手之前的方法。。");
+                System.out.println("执行了握手之前的方法。。");
+                return super.beforeHandshake(request, response, wsHandler, attributes);
+            }
+
+            @Override
+            public void afterHandshake(
+                    ServerHttpRequest request,
+                    ServerHttpResponse response,
+                    WebSocketHandler wsHandler,
+                    Exception ex) {
+
+                System.out.println("执行了握手之后的方法。。");
+                System.out.println("执行了握手之后的方法。。");
+                super.afterHandshake(request, response, wsHandler, ex);
+            }
+        };
     }
 
 }
