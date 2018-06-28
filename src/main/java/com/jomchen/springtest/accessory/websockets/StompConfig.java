28384befcf49fa9150f19c7a776da2cf51335fe1
Jomchen <937574740@qq.com>
2018-06-28 18:03:08 +0800

    增加了实验

---
+++ src/main/java/com/jomchen/springtest/accessory/websockets/StompConfig.java
@@ -0,0 +1,63 @@
+/*
+package com.jomchen.springtest.accessory.websockets;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
+import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+import java.util.List;
+
+*/
+/**
+ * create by Jomchen on 2018/6/28
+ *//*
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompConfig implements WebSocketMessageBrokerConfigurer  {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/portfolio")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic", "/queue");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        return true;
+    }
+
+}
+*/
---
+++ src/main/java/com/jomchen/springtest/controllers/websocket/WebSocketController.java
@@ -0,0 +1,20 @@
+/*
+package com.jomchen.springtest.controllers.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+*/
+/**
+ * create by Jomchen on 2018/6/28
+ *//*
+
+@Controller
+public class WebSocketController {
+
+    @MessageMapping("/greeting")
+    public String handle(String greeting) {
+        return "[" + System.currentTimeMillis() + ":" + greeting + "]";
+    }
+}
+*/
